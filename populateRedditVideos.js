77bf28fa1906e640cd2d9ee03f003f5e32440f9c
nosekp <glo3025pando@gmail.com>
2025-10-27 16:36:21 -0300

    finish

--- populateRedditVideos.js
+++ populateRedditVideos.js
@@ -21,29 +21,28 @@ async function populateSubreddit(subreddit, limit = 10) {
     for (const p of posts.slice(0, limit)) {
       const d = p.data;
       let mediaUrl = null;
-      console.log("daddy", d?.url_overridden_by_dest);
       if (d?.url_overridden_by_dest) {
         mediaUrl = await convertAndUpload(d.url, d.title);
         mediaType = "video";
       }
-      // 2. Videos nativos de Reddit
+
       else if (d.preview?.reddit_video_preview?.hls_url || d.secure_media?.reddit_video?.hls_url) {
         const hlsUrl = d.preview?.reddit_video_preview?.hls_url || d.secure_media?.reddit_video?.hls_url;
         mediaUrl = await convertAndUpload(hlsUrl, d.title);
         mediaType = "video";
       }
-      // 3. Imágenes directas
+
       else if (d.post_hint === 'image' && d.url.match(/\.(jpg|png|gif)$/i)) {
         mediaUrl = await uploadImage(d.url, d.title);
         mediaType = "image";
       }
-      // 4. Galerías (toma la primera imagen)
+
       else if (d.gallery_data && d.media_metadata) {
         const firstImg = Object.values(d.media_metadata)[0];
         mediaUrl = await uploadImage(firstImg.s.u.replace(/&amp;/g, '&'), d.title);
         mediaType = "image";
       }
-      // 5. Fallback a preview image
+
       else if (d.preview?.images?.[0]?.source?.url) {
         mediaUrl = await uploadImage(d.preview.images[0].source.url.replace(/&amp;/g, '&'), d.title);
         mediaType = "image";
@@ -68,4 +67,4 @@ async function populateSubreddit(subreddit, limit = 10) {
   }
 }
 
-populateSubreddit('BBC_Galore', 3);
+populateSubreddit('gamedevscreens', 5);
--- supabaseclient.js
+++ supabaseclient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import 'dotenv/config'
 
-const SUPABASE_URL = 'https://evmvrcuagrzxwiqacnik.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2bXZyY3VhZ3J6eHdpcWFjbmlrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MDcwMzkxNywiZXhwIjoyMDc2Mjc5OTE3fQ.w2JWyM7haU-8ypI3R2lYRgwyQh4t6_hvsSS18e_-ZCk'; // service role key
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY // service role key
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
